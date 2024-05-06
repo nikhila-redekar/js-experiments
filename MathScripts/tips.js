f7bd941c399018a9d8525578f017fa009ee25306
Nikhila <nikhila-redekar@pluralsight.com>
2024-05-06 23:29:23 +0530

    added hello sample inputs math program

---
+++ MathScripts/tips.js
@@ -0,0 +1,10 @@
+// sample inputs
+let foodCost = 79.25;
+let tax = 6.54;
+let tip = 12.00;
+
+// calculations
+let totalDue = foodCost + tax + tip;
+
+// output
+console.log("The total due is " + totalDue);
