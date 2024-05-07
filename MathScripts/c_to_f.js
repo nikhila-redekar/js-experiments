7f8225686ece0006644d3fea462e2ed108255165
Nikhila <nikhila-redekar@pluralsight.com>
2024-05-07 13:47:10 +0530

    added celsius to farenhit

---
+++ MathScripts/c_to_f.js
@@ -0,0 +1,9 @@
+// Function to convert Celsius to Fahrenheit
+function celsiusToFahrenheit(celsius) {
+    return (celsius * 9/5) + 32;
+}
+
+// Example usage
+const celsius = 30; // You can change the Celsius value to any value you want
+const fahrenheit = celsiusToFahrenheit(celsius);
+console.log(celsius + " degrees Celsius is equal to " + fahrenheit + " degrees Fahrenheit.");
